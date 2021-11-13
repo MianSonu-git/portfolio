a1851e0b2c9e2cfb742064c39799cb1ff59e4d46
MianSonu-git <miantalhan028989@gmail.com>
2021-11-13 20:39:54 +0500

    ""

--- src/Components/Header.component/Header.component.jsx
+++ src/Components/Header.component/Header.component.jsx
@@ -1,12 +1,19 @@
 import "./Header.component.style.css";
-import React from "react";
+import React,{useState} from "react";
+
 
 
 const HeaderComponents = () => {
+    const [isCheck,setCheck] = useState(false);
+    const onchangeCheckbox=()=>{      
+        setCheck(!isCheck);
+    }
     return (
         <>
             <div className="bodyContainer">
-                <div className="leftSide headerLeft row">
+            {
+                isCheck? (
+                    <div className={`${isCheck} leftSide headerLeft row`}>
                     <div className="headerText leftText text-center">
                         <h3 className="textContent col-md-12 col-sm-12">
                             <span className="spanText">Hi!</span>
@@ -14,7 +21,21 @@ const HeaderComponents = () => {
                         </h3>
                         <button className="btn btn-primary col-sm-12 col-md-12">Explore More...</button>
                     </div>
+                    <input type="checkbox" checked={isCheck} onChange={onchangeCheckbox}/>
                 </div>
+                ):(
+                    <div className="leftSide headerLeft row">
+                    <div className="headerText leftText text-center">
+                        <h3 className="textContent col-md-12 col-sm-12">
+                            <span className="spanText">Hi!</span>
+                            Wellcome to <span className="spanText">Point</span>
+                        </h3>
+                        <button className="btn btn-primary col-sm-12 col-md-12">Explore More...</button>
+                    </div>
+                    <input type="checkbox" checked={isCheck} onChange={onchangeCheckbox}/>
+                </div>
+                )
+            }
                 <div className="rightSide headerRight">
 
                 </div>
